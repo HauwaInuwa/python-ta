f0ee843731f47ef9b40c31201802da40f2027401
HauwaInuwa <inuwawalida@gmail.com>
2022-06-10 14:09:36 +0100

    source code added

--- src/app.py
+++ src/app.py
@@ -4,6 +4,16 @@ import os
 
 def prime(s):
     # your code goes here
+	if s>1:
+		for n in range (2,s):
+			if(s % n)==0:
+				return False
+			return True
+		else:
+			return False
+numb = input ("Enter a number: ")	
+s = int(numb)		
+print(prime(s))
 
 def solution(s):
     return prime(s)
@@ -11,6 +21,6 @@ def solution(s):
 
 if __name__ == "__main__":
     if len(sys.argv) <= 1:
-        sys.exit(os.error("Argument required"))
+        sys.exit(os.error())
 
     print(solution(sys.argv[1]))
